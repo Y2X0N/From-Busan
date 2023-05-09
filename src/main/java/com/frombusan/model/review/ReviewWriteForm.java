4a755f43fdbbc7b1cd752c4322e583d63e5d15d9
GHLee1 <wed8337@gmail.com>
2023-05-09 18:19:38 +0900

    [UPDATE] summernote test

--- src/main/java/com/frombusan/model/review/ReviewWriteForm.java
+++ src/main/java/com/frombusan/model/review/ReviewWriteForm.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class ReviewWriteForm {
-	  @NotBlank(message = "제목을 입력해주세요.")
+	  	@NotBlank(message = "제목을 입력해주세요.")
 	    private String title;
 
 	    @NotBlank(message = "내용을 입력해주세요.")
@@ -17,36 +17,11 @@ public class ReviewWriteForm {
 	    @NotBlank(message = "장소를 입력해주세요.")
 	    private String review_place;
 
-	    public String getTitle() {
-	        return title;
-	    }
-
-	    public void setTitle(String title) {
-	        this.title = title;
-	    }
-
-	    public String getContents() {
-	        return contents;
-	    }
-
-	    public void setContents(String contents) {
-	        this.contents = contents;
-	    }
-
-	    public String getReview_place() {
-	        return review_place;
-	    }
-
-	    public void setReview_place(String review_place) {
-	        this.review_place = review_place;
-	    }
-    
-
-    public static Review toReview(ReviewWriteForm reviewWriteForm) {
+	    public static Review toReview(ReviewWriteForm reviewWriteForm) {
     	Review review = new Review();
     	review.setTitle(reviewWriteForm.getTitle());
     	review.setContents(reviewWriteForm.getContents());
     	review.setReview_place(reviewWriteForm.getReview_place());
         return review;
-    }
+	    }
 }
