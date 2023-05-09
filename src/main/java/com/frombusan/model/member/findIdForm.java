2a7522920569b401d278799a2dfc738ca9f64dc4
lhps1234 <lhps1234@gmail.com>
2023-05-09 13:16:44 +0900

    [ADD]

---
+++ src/main/java/com/frombusan/model/member/findIdForm.java
@@ -0,0 +1,10 @@
+package com.frombusan.model.member;
+import lombok.Data;
+
+@Data
+public class findIdForm {
+    private String name;
+    private String phone_number;
+    private String password;
+    private String member_id;
+}
