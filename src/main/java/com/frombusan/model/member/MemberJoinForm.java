7f5d56f7a01d240b259e37071f7db7f73e21462d
silkroad94 <daisyluv3786@gmail.com>
2023-05-16 13:34:27 +0900

    [STYLE] css

--- src/main/java/com/frombusan/model/member/MemberJoinForm.java
+++ src/main/java/com/frombusan/model/member/MemberJoinForm.java
@@ -9,9 +9,10 @@ import java.time.LocalDate;
 @Data
 public class MemberJoinForm {
     
+	@Size(min = 5, max = 10)
     private String member_id;
     
-    @Size(min = 4, max = 20)
+    @Size(min = 5, max = 10)
     private String password;
     
     @NotBlank
