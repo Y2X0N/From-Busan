12407d83ea4616404af7a2de5ccbc32ed66e1b77
GHLee1 <wed8337@gmail.com>
2023-05-16 17:39:28 +0900

    [UPDATE] trans

--- src/main/java/com/frombusan/controller/TranslatorController.java
+++ src/main/java/com/frombusan/controller/TranslatorController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -106,6 +107,19 @@ public class TranslatorController {
 		
         return ResponseEntity.ok(joined);
     }
+	@PostMapping("tourMaps")
+	public ResponseEntity<List<Translation>> tourMaps(@RequestParam String lang,
+			@RequestParam(value="mainTitle[]", defaultValue="") List<String> mainTitle
+			)
+					throws IOException{
+		log.info("test{}",mainTitle);
+		List<Translation> joined = new ArrayList<>();
+		List<Translation> mainTitles = mmt.translate("ko",lang,mainTitle);
+		
+		joined.addAll(mainTitles);
+		
+		return ResponseEntity.ok(joined);
+	}
 
 	
 	@PostMapping("fesInfo")
