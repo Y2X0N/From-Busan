25694d15476ea5cc708de226ad0bd19668057495
silkroad94 <daisyluv3786@gmail.com>
2023-05-18 11:29:34 +0900

    [STYLE] reviewList

--- src/main/java/com/frombusan/controller/ReviewController.java
+++ src/main/java/com/frombusan/controller/ReviewController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DuplicateKeyException;
