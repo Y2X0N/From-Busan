548506f3e136ccbfec61a78f626924d40eb84948
ghlee1 <wed8337@gmail.com>
2025-03-17 23:29:05 +0900

    dto수정

--- server/src/main/java/com/frombusan/dto/tourist/TouristListDto.java
+++ server/src/main/java/com/frombusan/dto/tourist/TouristListDto.java
@@ -3,9 +3,12 @@ package com.frombusan.dto.tourist;
 import com.frombusan.model.tourist.Tourist_Spot;
 import com.frombusan.util.PageNavigator;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
+@Getter
+@Setter
 @AllArgsConstructor
 public class TouristListDto {
 
